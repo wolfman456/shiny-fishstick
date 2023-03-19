a8a5253d0150eb27433fbfffd5169b47d89e2d1c
wolfman456 <pshook456@gmail.com>
2023-03-18 22:39:05 -0500

    api skeleton added

--- routes/routes.js
+++ routes/routes.js
@@ -3,3 +3,32 @@ const express = require('express');
 const router = express.Router()
 
 module.exports = router;
+
+//test endpoint
+router.get("/hello",(req,res)=>{
+    res.send("Hello, World");
+})
+//Post Method
+router.post('/post', (req, res) => {
+    res.send('Post API')
+})
+
+//Get all Method
+router.get('/getAll', (req, res) => {
+    res.send('Get All API')
+})
+
+//Get by ID Method
+router.get('/getOne/:id', (req, res) => {
+    res.send('Get by ID API')
+})
+
+//Update by ID Method
+router.patch('/update/:id', (req, res) => {
+    res.send('Update by ID API')
+})
+
+//Delete by ID Method
+router.delete('/delete/:id', (req, res) => {
+    res.send('Delete by ID API')
+})
