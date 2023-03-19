4422b632b5c41a445494fdf15f39d183b43c3c5b
wolfman456 <pshook456@gmail.com>
2023-03-19 16:21:50 -0500

    basic api completed

---
+++ model/model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const dataSchema = new mongoose.Schema({
+    name:{
+        required:true,
+        type: String
+    },
+    age:{
+        required: true,
+        type:Number
+    },
+    salary:{
+        required:true,
+        type:Boolean
+    },
+    title:{
+        required:true,
+        type:String
+    }
+})
+
+module.exports = mongoose.model('Data', dataSchema)
--- routes/routes.js
+++ routes/routes.js
@@ -3,32 +3,75 @@ const express = require('express');
 const router = express.Router()
 
 module.exports = router;
-
+const Model = require('../model/model');
 //test endpoint
-router.get("/hello",(req,res)=>{
+router.get("/hello", (req, res) => {
     res.send("Hello, World");
 })
 //Post Method
-router.post('/post', (req, res) => {
-    res.send('Post API')
+router.post('/post', async (req, res) => {
+    const data = new Model({
+        name: req.body.name,
+        age: req.body.age,
+        salary: req.body.salary,
+        title: req.body.title
+    })
+    try {
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave)
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
 })
 
 //Get all Method
-router.get('/getAll', (req, res) => {
-    res.send('Get All API')
+router.get('/getAll', async (req, res) => {
+    try{
+        const data = await Model.find();
+        res.json(data)
+    }
+    catch (error){
+        res.status(500).json({message: error.message})
+    }
 })
 
 //Get by ID Method
-router.get('/getOne/:id', (req, res) => {
-    res.send('Get by ID API')
+router.get('/getOne/:id', async (req, res) => {
+    try{
+        const data = await Model.findById(req.params.id);
+        res.json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
 })
 
 //Update by ID Method
-router.patch('/update/:id', (req, res) => {
-    res.send('Update by ID API')
+router.patch('/update/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedData = req.body;
+        const options = { new: true };
+
+        const result = await Model.findByIdAndUpdate(
+            id, updatedData, options
+        )
+
+        res.send(result)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
 })
 
 //Delete by ID Method
-router.delete('/delete/:id', (req, res) => {
-    res.send('Delete by ID API')
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const data = await Model.findByIdAndDelete(id)
+        res.send(`Document with ${data.name} has been deleted..`)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
 })
